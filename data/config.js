d97e10b95d114d0c4eee1a6acd841cad11779c7e
swxppy <swapsnil12@gmail.com>
2023-03-04 10:37:10 +0530

    datasets updated

---
+++ data/config.js
@@ -0,0 +1,88 @@
+const categories = [
+  {
+    name: 'Hardware',
+    items: [
+      {
+        title: 'Lenovo Legion 5i 15IA7H',
+        description:
+          'The i7-12700H chip is just amazing. Plus, 16 GB RAM and 2TB storage. And RTX 3070 ;)',
+        url: 'https://www.lenovo.com/in/en/laptops/legion-laptops/legion-5-series/Legion-5i-Gen-7-15-inch-Intel/p/LEN101G0016',
+      },
+      {
+        title: 'Lenovo IdeaPad Flex 5 141TL05',
+        description:
+          'Has an i7-1165G7 chipset with 16 GB RAM and 512 GB of storage. Intel Iris XE Graphics',
+        url: 'https://www.flipkart.com/lenovo-ideapad-flex-5-core-i7-11th-gen-16-gb-512-gb-ssd-windows-10-home-14itl05-2-1-laptop/p/itm331dc6a582645 ',
+      },
+      {
+        title: 'Wacom Intuos S CTL-4100',
+        description: 'Wacom Intuos creative pen tablet small, with pressure-sensitive, battery-free Wacom Pen 4K',
+        url: 'https://india-iws.in/products/intuos-small-without-bluetooth',
+      },
+      {
+        title: 'Lenovo Legion M300 Mouse',
+        description: 'Ergonomic, ambidextrous design, 8 easily programmable buttons, an ultra-precise 8,000 DPI adjustable sensor, and customisable RGB LED backlighting on the logo',
+        url: 'https://www.lenovo.com/in/en/accessories-and-monitors/gaming-accessories/MICE-BO-Lenovo-M300-RGB-Gaming-Mouse/p/GY50X79384h',
+      },
+      {
+        title: 'Blaupunkt BH51 Moksha ANC',
+        description: 'ANC, 40MM drivers, 32H of playtime and uses Bluetooth 5.0',
+        url: 'https://blaupunktaudio.in/products/bh51-anc-bk',
+      },
+    ],
+  },
+  {
+    name: 'Software',
+    items: [
+      {
+        title: 'Visual Studio Code',
+        description:
+          'Preferred Code Editor',
+        url: 'https://www.adobe.com/in/products/photoshop.html',
+      },
+      {
+        title: 'Adobe Photoshop',
+        description:
+          'My go-to image editing and graphic designing software',
+        url: 'https://www.adobe.com/in/products/photoshop.html',
+      },
+      {
+        title: 'Adobe Illustrator',
+        description:
+          'Primarily using it for creating simple vector art',
+        url: 'https://www.adobe.com/in/products/illustrator.html',
+      },
+      {
+        title: 'Clip Studio Paint',
+        description:
+          'Intend to use it as my primary graphic software once I learn it properly',
+        url: 'https://www.adobe.com/in/products/illustrator.html',
+      },
+    ],
+  },
+  {
+    name: 'Operating System',
+    items: [
+      {
+        title: 'Windows 11',
+        description:
+          'Preferred OS for all my work and hobbies',
+        url: 'https://www.microsoft.com/en-in/windows/windows-11',
+      },
+      {
+        title: 'Ubuntu 20.04',
+        description:
+          'Using it as my OS for Windows Subsystem for Linux',
+        url: 'https://ubuntu.com/',
+      },
+      {
+        title: 'Arch Linux',
+        description:
+          'The OS I keep on my disk for dual booting. Used for hobbyist pursuits.',
+        url: 'https://archlinux.org/',
+      },
+    ],
+  },
+]
+
+export default categories
